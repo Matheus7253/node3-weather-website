5241a1f19ef7796bc7b01ae2649f3100b392826e
Matheus7253 <100863284+Matheus7253@users.noreply.github.com>
2022-05-05 11:49:19 -0400

    Added humidity to forecast

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -1,5 +1,6 @@
 const request = require("request");
 
+//
 const forecast = (latitude, longitude, callback) => {
   const url =
     "http://api.weatherstack.com/current?access_key=25357963f48e95a78dde20b7936823ce&query=" +
@@ -19,9 +20,11 @@ const forecast = (latitude, longitude, callback) => {
         body.current.weather_descriptions[0] +
           ". It is currently " +
           body.current.temperature +
-          " degrees. It feels like " +
+          " degrees out. It feels like " +
           body.current.feelslike +
-          " degrees out."
+          " degrees out. The humidity is " +
+          body.current.humidity +
+          "%."
       );
     }
   });
